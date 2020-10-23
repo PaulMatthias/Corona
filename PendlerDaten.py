096f32b84d1acc354fdec8188838f0a046f75a32
Matthias, Paul (EXTERN: CGI) <paul.matthias@cgi.com>
2020-10-23 10:29:00 +0200

    add Pendlerdaten changes

--- PendlerDaten.py
+++ PendlerDaten.py
@@ -1,6 +1,10 @@
 import pandas as pd 
 
 dfList = []
+listOfBundeslaender = ["Schleswig-Holstein", "Brandenburg", "Mecklenburg-Vorpommern", "Rheinland-Pfalz", "Nordrhein-Westfalen", "Sachsen", "Sachsen-Anhalt", "Hessen",\
+    "Baden-Württemberg", "Bayern", "Saarland", "Thüringen", "Niedersachsen", "Übrige Kreise (Regierungsbezirk)", "Übrige Regierungsbezirke (Bundesland)", "Auspendler in das Bundesgebiet",\
+        "Auspendler insgesamt"]
+
 
 for i in range(1,17):
     if i < 10:
@@ -15,13 +19,23 @@ for i in range(1,17):
 
     homeNode="Nirvana"
     for ind in df.index:
-        if df['homeNode'][ind] is str:
+        if isinstance(df['homeNode'][ind], str):
             homeNode = df['homeNode'][ind]
         else:
-            df.loc['homeNode', ind] = homeNode
+            df.at[ind, 'homeNode'] = homeNode
 
-    df = df.iloc[0:]
+    #drop everything with nan
+    df = df.dropna()
+    #rm travell sums to other Bundeslaender
+    df = df[~df['workNode'].isin(listOfBundeslaender)]
+    #drop duplicate entries (dont know the reason, but there are some in the excel sheets)
+    df["workNode"] = df["workNode"].str.replace("Reg.-Bez.", "")
+    df["workNode"] = df["workNode"].str.replace("Statistische Region", "")
+  
+    df = df.drop_duplicates()
+  
+    df.to_excel("testTransferOf_"+str(i)+".xls")
+        
     dfList.append(df)
 
-print(dfList[0].columns)
-print(dfList[0][:12])
+print(dfList[0][:40])
