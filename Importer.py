05b0471d8fbfe3415301af7df90f4db60b114824
Matthias, Paul (EXTERN: CGI) <paul.matthias@cgi.com>
2020-10-26 14:30:33 +0100

    rename

--- Importer.py
+++ Importer.py
@@ -1,5 +1,15 @@
 import pandas as pd 
 
-dfTotalPeople = pd.read_csv("PendlerDaten/EinwohnerProKreis.csv")
+class InputData():
+    """
+    Data class which reads and stores all necessary input
+    """
+    def __init__(self):
+        self.dfTotalPeople = pd.read_csv("PendlerDaten/EinwohnerProKreis.csv")
 
-print(dfTotalPeople.head)
+        self.dfListOfBundesland = []
+
+        for i in range(1,17):
+            df=pd.read_excel('PendlerDaten//CleanDataOf_' + str(i) + '.xls')
+            self.dfListOfBundesland.append(df)
+        
